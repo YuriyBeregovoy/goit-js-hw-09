5488e0e27ee55f950d24612e4384dfa0e5e9b6e0
YuriyBeregovoy <rik0681@gmail.com>
2023-06-27 21:46:55 +0300

    added the code of the task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,27 @@
 
-
-
-
-
+const bodyBckgColorEl = document.querySelector("body");
+const startBtnChangeColorEl = document.querySelector("button[data-start]");
+const buttonStopColorEl = document.querySelector("button[data-stop]");
+let timerId = null;
 
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+startBtnChangeColorEl.addEventListener('click', () => {
+   timerId = setInterval(function () {
+     bodyBckgColorEl.style.backgroundColor = getRandomHexColor(); 
+   }, 1000)
+
+  startBtnChangeColorEl.disabled = true;
+  buttonStopColorEl.disabled = false;
+});
+
+buttonStopColorEl.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtnChangeColorEl.disabled = false;
+  buttonStopColorEl.disabled = true;
+});
+
+
